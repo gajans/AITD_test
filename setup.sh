b3bf785c3c1f7d7320cabdba424f779eeca20e50
gajan_nine <gajan.suthokumar@nine.com.au>
2024-02-21 10:25:38 +1100

    commit 2

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ set -e
 
 # download models
 #mkdir -p models/toxicity_model && cd models/toxicity_model && curl -L --output toxic_bias-4e693588.ckpt https://github.com/unitaryai/detoxify/releases/download/v0.1-alpha/toxic_bias-4e693588.ckpt  && cd ../..
-mkdir -p models/toxicity_model/transformers && cd models/toxicity_model/transformers && git clone --depth 1 https://huggingface.co/roberta-base && cd ../../..
+#mkdir -p models/toxicity_model/transformers && cd models/toxicity_model/transformers && git clone --depth 1 https://huggingface.co/roberta-base && cd ../../..
 mkdir -p models/transformers && cd models/transformers && git clone --depth 1 https://huggingface.co/bhadresh-savani/distilbert-base-uncased-emotion && cd ../..]
